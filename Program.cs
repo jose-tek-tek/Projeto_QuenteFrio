b0a15f85192f3ab09b9cd0798ba37ab9fd55cfb4
jose-tek-tek <163602203+jose-tek-tek@users.noreply.github.com>
2024-05-30 21:54:53 -0300

    Add files via upload
    Projeto "QuenteFrio"

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Random numAleatorio = new Random();
+int valorInteiro = numAleatorio.Next(1,100);
+int palpite;
+
+Console.WriteLine("--- ADIVINHE O NÚMERO ---\n");
+Console.ReadKey();
+
+Console.WriteLine("Gerando um número...");
+Thread.Sleep(3000);
+Console.WriteLine("Número Sorteado!!!");
+Thread.Sleep(1000);
+
+    do {
+            
+            Console.Write("Adivinhe o número que estou pensando!......: ");
+            palpite = int.Parse(Console.ReadLine()!);
+
+            if (palpite < valorInteiro){
+                Console.WriteLine("Está Frio!!");
+            }
+
+            else if (palpite > valorInteiro){
+                Console.WriteLine("Está Quente!!");
+            }
+
+            else{
+                Console.WriteLine("Parabéns! Você acertou o número!");
+            }
+
+        } while (palpite != valorInteiro);
+
+    Console.WriteLine("O número era: " + valorInteiro);
+    Thread.Sleep(5000);
